08f565c0b4dcb283601c3449c865e530b4574207
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-06-06 20:12:52 +0800

    網頁切版 CSS + 配合 CSS 調整局部元件 + 新增清除 CSS 樣式的 CSS

--- src/navbar.js
+++ src/navbar.js
@@ -6,11 +6,13 @@ function generateNavBar() {
 
     // 建立 navbar 容器（提供 css 切版）
     const navbarDiv = document.createElement('div');
-    navbarDiv.className = 'header';
+    navbarDiv.className = 'navbarDiv';
 
     // 建立 logo 區域容器 
     const logoDiv = document.createElement('a');
     logoDiv.href = "./index.html"
+    logoDiv.className = "logoDiv";
+
     // 建立 logo 要用的圖片 *2
     const logopics = document.createElement('img');
     logopics.src = "https://picsum.photos/67";
@@ -21,16 +23,28 @@ function generateNavBar() {
     logoDiv.appendChild(logopics);
     logoDiv.appendChild(logotitle);
     
+    
     // 建立網頁分頁容器
     const tabDiv = document.createElement('div');
+    tabDiv.className = "tabDiv"
     // 建立分頁子項目
     const linkAbout = document.createElement('a');
     linkAbout.textContent = "關於我們";
     linkAbout.className = "linkName";
     linkAbout.href = "./about.html"
+    const linkGame = document.createElement('a');
+    linkGame.textContent = "香料遊戲";
+    linkGame.className = "linkName";
+    linkGame.href = "#";
+    const linkRecipe = document.createElement('a');
+    linkRecipe.textContent = "香料食譜";
+    linkRecipe.className = "linkRecipe";
+    linkRecipe.href = "#";
 
     // 把分頁超連結加入分頁容器中
     tabDiv.appendChild(linkAbout);
+    tabDiv.appendChild(linkGame);
+    tabDiv.appendChild(linkRecipe);
 
     // 導覽列最底部的 © 2023 GingerLemon.
     const copyright = document.createElement('p');
--- src/searchResult.js
+++ src/searchResult.js
@@ -7,44 +7,54 @@ function generateSpeciesResult() {
 
     // 建立基本資料節點
     const infoBox = document.createElement('div');
+    infoBox.id = "infoBox";
+    infoBox.className = "infoBox";
+
 
 
     // 建立搜尋香料名稱節點
     const taxonNameDiv = document.createElement('div');
+    taxonNameDiv.className = "taxonNameDiv";
     
     // 建立子節點
-    const speciesName = document.createElement('p');
-    speciesName.className = "speciesName"; 
-    speciesName.textContent = "Litsea cubeba"; // 測試用
-    const scientificName = document.createElement('p');
-    scientificName.className = "scientificName";
-    scientificName.textContent = "山胡椒"; // 測試用
-    const decoline = document.createElement('bra');
+    const scientificName= document.createElement('p');
+    scientificName.className = "scientificName"; 
+    scientificName.textContent = "Litsea cubeba"; // 測試用
+    const speciesName  = document.createElement('p');
+    speciesName.className = "speciesName";
+    speciesName.textContent = "山胡椒"; // 測試用
+    const decoline = document.createElement('br');
 
-    taxonNameDiv.appendChild(speciesName);
     taxonNameDiv.appendChild(scientificName);
+    taxonNameDiv.appendChild(speciesName);
     taxonNameDiv.appendChild(decoline);
 
 
     // 建立原生/特有種等標籤
     const taxonTagDiv = document.createElement('div'); 
-    
+    taxonTagDiv.className = "taxonTagDiv";
+
     // 建立標籤
     const speciesTag = document.createElement('div');
     speciesTag.className = "speciesTag"; // 測試用
+    const tagname = document.createElement('p');
     speciesTag.textContent = "原生種"; // 測試用
 
+    speciesTag.appendChild(tagname);
     taxonTagDiv.appendChild(speciesTag); // 測試用
  
 
     // 建立分類與別稱
     const taxonotherDiv = document.createElement('div');
+    taxonotherDiv.className = "taxonotherDiv";
     
     // 建立子節點
     const levelArea = document.createElement('p');
+    levelArea.className = "levelArea";
     const level = ["樟科", "木子薑屬"]; // 測試用
     levelArea.textContent = level.join(">");
     const aliasArea = document.createElement('p');
+    aliasArea.className = "aliasArea";
     const alias = [
         "Litsea citrata Blume", 
         "Actinodaphne citrata (Blume) Hayata"]; //測試用
@@ -67,18 +77,24 @@ function generateSpeciesResult() {
 
     // 資訊卡常態出現部分
     const moreInfoCard = document.createElement('div');
+    moreInfoCard.className = "moreInfoCard"
     
 
     const speciesPic = document.createElement('img');
-    speciesPic.src="http://kplant.biodiv.tw/%E5%B1%B1%E8%83%A1%E6%A4%92/%E5%B1%B1%E8%83%A1%E6%A4%9201.jpg"; // 測試用
+    speciesPic.src="https://picsum.photos/120"; // 測試用
+    speciesPic.className = "speciesPic";
     const speciesCommonName = document.createElement('p');
+    speciesCommonName.className = "speciesCommonName";
     const commonName = ["山雞椒、MaKao（泰雅族）", "Maqrig（太魯閣族）", "Mae’aew （賽夏族）"]; // 測試用
-    speciesCommonName.textContent = "又稱：" + commonName.join("、"); // 測試用
+    speciesCommonName.textContent = "又稱："  + commonName.join("、"); // 測試用
   
     // 右上角按鈕
     const cardBtn = document.createElement('div');
-    cardBtn.textContent = "∧"; 
     cardBtn.className = "cardBtn";
+    const cardBtnText = document.createElement('p');
+    cardBtnText.textContent = "∧"; 
+
+    cardBtn.appendChild(cardBtnText);
 
     moreInfoCard.append(speciesPic);
     moreInfoCard.append(speciesCommonName);
@@ -91,14 +107,17 @@ function generateSpeciesResult() {
 
     // 擴充部分 
     const moreTexts01 = document.createElement('p');
+    moreTexts01.className = "moreTexts";
     const moreTexts02 = document.createElement('p');
+    moreTexts02.className = "moreTexts";
     const moreTexts03 = document.createElement('p');
+    moreTexts03.className = "moreTexts";
     const speciesProperty = "分佈於台灣 1000 公尺上下中低海拔地區；開花期為 2-4 月，結果期為 6-8 月。"; // 測試用
     const speciesFeature = "有薑與胡椒的香氣，全株皆可食用。果實曬乾後成紫黑色，外型似黑胡椒粒。"; // 測試用
     const speciesApply = "花朵可泡茶、嫩葉可入菜，許多原住民族將種子當成去腥的香料或鹽巴的替代品。";
-    moreTexts01.textContent = "特性｜" + speciesProperty;
-    moreTexts02.textContent = "特徵｜" + speciesFeature;
-    moreTexts03.textContent = "應用｜" + speciesApply;
+    moreTexts01.textContent = "特性｜ " + speciesProperty;
+    moreTexts02.textContent = "特徵｜ " + speciesFeature;
+    moreTexts03.textContent = "應用｜ " + speciesApply;
     
     // 擴充部分加入資訊卡
     moreInfoTexts.appendChild(moreTexts01);
