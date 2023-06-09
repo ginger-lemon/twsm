9ebdff6736993e8241e385c339cb5ff3eec95457
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-06-09 12:13:32 +0800

    整理用 js 生成 DOM node 的 js 檔案、微調 html 檔案與 class 名稱

--- src/navbar.js
+++ src/navbar.js
@@ -1,62 +1,33 @@
 // 導覽列元件
 
+// 取得要插入的容器
 function generateNavBar() {
-    // 取得要插入的容器
     const container = document.getElementById('navbarContainer');
 
-    // 建立 navbar 容器（提供 css 切版）
+    // 建立 navbar 容器集中管理各項元素
     const navbarDiv = document.createElement('div');
-    navbarDiv.className = 'navbarDiv';
+    navbarDiv.className = "navbarDiv";
 
+    // 建立 navbar 底下的 HTML 標籤
+    navbarDiv.innerHTML = `
+        <div class="logoDiv">
+            <a href="index.html">
+                <img 
+                    src="./src/img/logo.svg" width="96px"
+                    height="132px"
+                />
+            </a>
+        </div>
+        <div class="tabDiv">
+            <a href="./about.html">關於我們</a>
+            <a href="#">香料遊戲</a>
+            <a href="#">香料食譜</a>
+        </div>
+        <div class="copyright">
+            ©2023 GingerLemon.
+        </div>
+    `;
 
-    // 建立 logo 區域容器 
-    const logoDiv = document.createElement('div');
-    logoDiv.className = "logoDiv";
-
-    // 建立一個 a 裡面放圖片讓使用者點擊圖片可以連到首頁
-    const logolink = document.createElement('a');
-    logolink.href = "index.html";
-    const logo = document.createElement('img');
-    logo.src = "./src/img/logo.svg";
-
-    logolink.appendChild(logo);
-
-    // 加入 logoDiv 內
-    logoDiv.appendChild(logolink);
-    
-    
-    // 建立網頁分頁容器
-    const tabDiv = document.createElement('div');
-    tabDiv.className = "tabDiv"
-    // 建立分頁子項目
-    const linkAbout = document.createElement('a');
-    linkAbout.textContent = "關於我們";
-    linkAbout.href = "./about.html"
-    const linkGame = document.createElement('a');
-    linkGame.textContent = "香料遊戲";
-    linkGame.href = "#";
-    const linkRecipe = document.createElement('a');
-    linkRecipe.textContent = "香料食譜";
-    linkRecipe.href = "#";
-
-    // 把分頁超連結加入分頁容器中
-    tabDiv.appendChild(linkAbout);
-    tabDiv.appendChild(linkGame);
-    tabDiv.appendChild(linkRecipe);
-
-    // 導覽列最底部的 © 2023 GingerLemon.
-    const copyright = document.createElement('div');
-    copyright.textContent = "©2023 GingerLemon.";
-    copyright.className = "copyright";
-
-    // 把全部節點加入到 navbarDiv 內
-    navbarDiv.appendChild(logoDiv);
-    navbarDiv.appendChild(tabDiv);
-    navbarDiv.appendChild(copyright);
-
-    // 把 navbarDiv 加入到 navbarContainer 內
+    // 加入到容器內
     container.appendChild(navbarDiv);
-
 }
-
-
--- src/searchResult.js
+++ src/searchResult.js
@@ -1,138 +1,104 @@
-// 取得下拉式選單結果後才會顯示的結果 
-
-function generateSpeciesResult() {
-    // 取得要插入的容器
-    const container = document.getElementById('resultContainer');
+// 中間下拉式選單與結論部分
 
 
-    // 建立基本資料節點
+// 下拉式選單
+function generateDorpdowList() {
+    const droplistContainer = document.getElementById('droplistDiv');
+
+    const searchList = document.createElement('select');
+    searchList.className = "searchList";
+    searchList.id = "searchList";
+    searchList.innerHTML = `
+        <option>今天想找什麼香料呢？</option>
+        <option value="山胡椒">山胡椒 / 馬告</option>
+        <option value="食茱萸">食茱萸 / 刺蔥</option>
+        <option value="土肉桂">土肉桂</option>
+        <option value="土當歸">土當歸</option>
+        <option value="羅氏鹽膚木">羅氏鹽膚木</option>
+        <option value="大葉楠果實">大葉楠果實</option>
+        <option value="月桃">月桃</option>
+        <option value="艾草">艾草</option>
+        <option value="紫蘇">紫蘇</option>
+        <option value="大葉石龍尾">大葉石龍尾</option>
+    `;
+
+    droplistContainer.appendChild(searchList);
+}
+
+// 顯示搜尋結果
+function generateSearchResult() {
+    // 選擇插入容器
+    const resultContainer = document.getElementById('resultContainer');
+    
     const infoBox = document.createElement('div');
-    infoBox.id = "infoBox";
     infoBox.className = "infoBox";
+    infoBox.innerHTML = `
+        <div class="nameDiv">
+            <p class="itailicName">Litsea cubeba（js 測試）</p>
+            <p class="speciesName">山胡椒（js 測試）</p>
+        </div> 
+        <div class="tagsDiv">
+            <div class="tags">原生種（js 測試）</div>
+        </div> 
+        <div class="classDiv">
+            <p>
+               <span>樟科（js 測試）</span> > <span>木子薑屬（js 測試）</span>
+            </p>
+        </div> 
+    `;
 
-
-
-    // 建立搜尋香料名稱節點
-    const taxonNameDiv = document.createElement('div');
-    taxonNameDiv.className = "taxonNameDiv";
-    
-    // 建立子節點
-    const scientificName= document.createElement('p');
-    scientificName.className = "scientificName"; 
-    scientificName.textContent = "Litsea cubeba"; // 測試用
-    const speciesName  = document.createElement('p');
-    speciesName.className = "speciesName";
-    speciesName.textContent = "山胡椒"; // 測試用
-    const decoline = document.createElement('br');
-
-    taxonNameDiv.appendChild(scientificName);
-    taxonNameDiv.appendChild(speciesName);
-    taxonNameDiv.appendChild(decoline);
-
-
-    // 建立原生/特有種等標籤
-    const taxonTagDiv = document.createElement('div'); 
-    taxonTagDiv.className = "taxonTagDiv";
-
-    // 建立標籤
-    const speciesTag = document.createElement('div');
-    speciesTag.className = "speciesTag"; // 測試用
-    const tagname = document.createElement('p');
-    speciesTag.textContent = "原生種"; // 測試用
-
-    speciesTag.appendChild(tagname);
-    taxonTagDiv.appendChild(speciesTag); // 測試用
- 
-
-    // 建立分類與別稱
-    const taxonotherDiv = document.createElement('div');
-    taxonotherDiv.className = "taxonotherDiv";
-    
-    // 建立子節點
-    const levelArea = document.createElement('p');
-    levelArea.className = "levelArea";
-    const level = ["樟科", "木子薑屬"]; // 測試用
-    levelArea.textContent = level.join(">");
-    const aliasArea = document.createElement('p');
-    aliasArea.className = "aliasArea";
-    const alias = [
-        "Litsea citrata Blume", 
-        "Actinodaphne citrata (Blume) Hayata"]; //測試用
-    aliasArea.textContent = "別名：" + alias.join("、"); // 測試用
-
-    taxonotherDiv.appendChild(levelArea);
-    taxonotherDiv.appendChild(aliasArea);
-    
-
-    // 把節點放入 infoBox
-    infoBox.appendChild(taxonNameDiv);
-    infoBox.appendChild(taxonTagDiv);
-    infoBox.appendChild(taxonotherDiv);
-
-
-
-    // 建立其他資訊卡
     const moreInfoBox = document.createElement('div');
     moreInfoBox.className = "moreInfoBox";
-
-    // 資訊卡常態出現部分
-    const moreInfoCard = document.createElement('div');
-    moreInfoCard.className = "moreInfoCard"
+    moreInfoBox.innerHTML = `
+        <div class="moreInfoCard">
+            <div class="fixedPart">
+                <img 
+                    src="https://picsum.photos/120"
+                    width="120px"
+                    height="120px"
+                />
+                <p>
+                    又稱：<br/>
+                    山雞椒、MaKao（泰雅族）、Maqrig（太魯閣族）、Mae’aew （賽夏族）。（js 測試）
+                </p>
+            </div>
+            <!-- 延展的部分 -->
+            <div class="extendanlePart">
+                <div>
+                    <!-- display: block; -->
+                    <p><b>特性｜ </b></p>
+                    <p>分佈於台灣 1000 公尺上下中低海拔地區；
+                    開花期為 2-4 月，結果期為 6-8 月。（js 測試）</p>
+                </div>
+                <div>
+                    <!-- display: block; -->
+                    <p><b>特徵｜ </b></p>
+                    <p>有薑與胡椒的香氣，全株皆可食用。果實曬
+                    乾後成紫黑色，外型似黑胡椒粒。（js 測試）</p>
+                </div>
+                <div>
+                    <!-- display: block; -->
+                    <p><b>應用｜ </b></p>
+                    <p>花朵可泡茶、嫩葉可入菜，許多原住民族將
+                    種子當成去腥的香料或鹽巴的替代品。（js 測試）</p>
+                </div>
+            </div>
+        </div>
+        <div class="moreInfoBtn" width="48px" height="48px">
+            <img 
+                src="https://picsum.photos/48"
+                class="btn-extend"
+            />
+            <img 
+                src="https://picsum.photos/48"
+                class="btn-close"
+            />
+        </div>
+    `;
     
-
-    const speciesPic = document.createElement('img');
-    speciesPic.src="https://picsum.photos/120"; // 測試用
-    speciesPic.className = "speciesPic";
-    const speciesCommonName = document.createElement('p');
-    speciesCommonName.className = "speciesCommonName";
-    const commonName = ["山雞椒、MaKao（泰雅族）", "Maqrig（太魯閣族）", "Mae’aew （賽夏族）"]; // 測試用
-    speciesCommonName.textContent = "又稱："  + commonName.join("、"); // 測試用
-  
-    // 右上角按鈕
-    const cardBtn = document.createElement('div');
-    cardBtn.className = "cardBtn";
-    const cardBtnText = document.createElement('p');
-    cardBtnText.textContent = "∧"; 
-
-    cardBtn.appendChild(cardBtnText);
-
-    moreInfoCard.append(speciesPic);
-    moreInfoCard.append(speciesCommonName);
-    moreInfoCard.append(cardBtn);
-    
-    
-    // 資訊卡點開擴充部分，平常的 display 會是 none
-    const moreInfoTexts = document.createElement('div');
-    moreInfoTexts.className = "moreInfoTexts";
-
-    // 擴充部分 
-    const moreTexts01 = document.createElement('p');
-    moreTexts01.className = "moreTexts";
-    const moreTexts02 = document.createElement('p');
-    moreTexts02.className = "moreTexts";
-    const moreTexts03 = document.createElement('p');
-    moreTexts03.className = "moreTexts";
-    const speciesProperty = "分佈於台灣 1000 公尺上下中低海拔地區；開花期為 2-4 月，結果期為 6-8 月。"; // 測試用
-    const speciesFeature = "有薑與胡椒的香氣，全株皆可食用。果實曬乾後成紫黑色，外型似黑胡椒粒。"; // 測試用
-    const speciesApply = "花朵可泡茶、嫩葉可入菜，許多原住民族將種子當成去腥的香料或鹽巴的替代品。";
-    moreTexts01.innerHTML = "特性｜ " + speciesProperty;
-    moreTexts02.textContent = "特徵｜ " + speciesFeature;
-    moreTexts03.textContent = "應用｜ " + speciesApply;
-    
-    // 擴充部分加入資訊卡
-    moreInfoTexts.appendChild(moreTexts01);
-    moreInfoTexts.appendChild(moreTexts02);
-    moreInfoTexts.appendChild(moreTexts03);
-    
-
-
-    // 加入到資訊卡中
-    moreInfoBox.appendChild(moreInfoCard);
-    moreInfoBox.appendChild(moreInfoTexts);
-    
-
-    // 將新增節點加入容器內 
-    container.appendChild(infoBox);
-    container.appendChild(moreInfoBox);
-
+    resultContainer.appendChild(infoBox);
+    resultContainer.appendChild(moreInfoBox);
 }
+
+
+
