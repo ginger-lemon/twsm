136b7490b9e933ac89c05a684fbad9027bc5ebfa
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-06-12 21:37:42 +0800

    調整 babel 2l7

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  // 綑綁時的模式： development/production
   mode: "development", 
   entry: './src/index.js', 
   output: { 
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js', 
-  },
-  // 客製化 server 
+    filename: 'bundle.js', 
+  }, 
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
@@ -16,39 +14,30 @@ module.exports = {
     compress: true,
     port: 8080,
   },
-  // 檔案讀取
   module: {
     rules: [
-      // Js 檔案 （babel）
       {
-        test: /\.(?:js|mjs|cjs)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
+            presets:[
+              "@babel/preset-env",
+              "@babel/preset-react"
+          ]
           }
         }
       },
-      //  CSS (css-loader)
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      // 讀取不同類型的檔案
       {
         test: /\.png/,
         type: 'asset/resource'
       },
     ]
   },
-
-  // 外掛工具
-
-  // plugins: [new HtmlWebpackPlugin({
-  //   template: './base.html'
-  // })],
 };
 
