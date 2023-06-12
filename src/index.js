376ca1c1c093b59157e2579faf5b77a4b7f34b80
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-06-12 20:19:15 +0800

    install babel for react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("開發環境初始化設定測試");
+import { createRoot } from "react-dom/client";
+
+// clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+
+const root = createRoot(document.getElementById("app"));
+root.render(<h2>For testing react dev environment</h2>);
