48907f1b15134172a045024f8539c53443bf282b
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-06-28 17:31:59 +0800

    加入套件 react-leaflet-cluster 整理地圖的點

--- src/component/appearmap.js
+++ src/component/appearmap.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyledMapContainer } from "../styles/Styled.map.js";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+
 
 export default function AppearMap({ species, mapData, isLoading }) {
 
@@ -39,17 +41,26 @@ export default function AppearMap({ species, mapData, isLoading }) {
                 <MapContainer
                 center={[23.697809, 120.960518]} 
                 zoom={8} 
-                scrollWheelZoom={false}
+                scrollWheelZoom={true}
             >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
+                <MarkerClusterGroup chunkedLoading>
                 {features.map((feature, i) => (
-                    <Marker key={i} position={[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]}>
-                        <Popup>{[feature.properties.lat, feature.properties.lng]}</Popup>
+                    <Marker 
+                        key={i} 
+                        position={[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]}
+                        // title={address[2]}
+                        // icon={customIcon}
+                    >
+                        <Popup>
+                            {"你可以在這裡看到：" + [feature.properties.lat, feature.properties.lng]}
+                        </Popup>
                     </Marker>
                 ))}
+                </MarkerClusterGroup>
         </MapContainer>
         </StyledMapContainer>
         );
